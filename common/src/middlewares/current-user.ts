aa27e85c1c3d95cf8c14a3762886ceaec825f5a2
Gervais Codes <gervaiscodes@protonmail.com>
2022-08-09 21:29:22 +0200

    Force release

--- common/src/middlewares/current-user.ts
+++ common/src/middlewares/current-user.ts
@@ -24,7 +24,7 @@ export const currentUser = (req: Request, res: Response, next: NextFunction) => 
     const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload
     req.currentUser = payload
   } catch {
-
+    console.log('Wrong JWT')
   }
 
   next()
