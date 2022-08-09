e801ba72985ffced21f8b01fd3ec1f4f9c6b838c
Gervais Codes <gervaiscodes@protonmail.com>
2022-08-09 23:27:42 +0200

    Show ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import cookieSession from 'cookie-session'
 import { NotFoundError, errorHandler, currentUser } from '@jd/ticketing-common'
 
 import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
 
 const app = express()
 app.set('trust proxy', true)
@@ -20,6 +21,7 @@ app.use(
 app.use(currentUser)
 
 app.use(createTicketRouter)
+app.use(showTicketRouter)
 
 app.all ('*', () => {
   throw new NotFoundError()
