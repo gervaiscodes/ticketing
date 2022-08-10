3e49a3d675f578582535f96d35fd8fc736b5b7c2
Gervais Codes <gervaiscodes@protonmail.com>
2022-08-10 10:15:18 +0200

    Update ticket route

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,94 @@
+import request from 'supertest'
+import { app } from '../../app'
+import mongoose from 'mongoose'
+
+it('returns a 404 if the provided id does not exist', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString()
+  await request(app)
+          .put(`/api/tickets/${id}`)
+          .set('Cookie', global.getAuthCookie())
+          .send({
+            title: 'ddsq',
+            price: 20
+          })
+          .expect(404)
+})
+
+it('returns a 401 if the user is not authenticated', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString()
+  await request(app)
+          .put(`/api/tickets/${id}`)
+          .send({
+            title: 'ddsq',
+            price: 20
+          })
+          .expect(401)
+})
+
+it('returns a 401 if the user does not own the ticket', async () => {
+  const createdTicket = await request(app).post('/api/tickets').set('Cookie', global.getAuthCookie()).send({
+    title: 'Concert',
+    price: 20
+  })
+
+  await request(app)
+          .put(`/api/tickets/${createdTicket.body.id}`)
+          .set('Cookie', global.getAuthCookie()) // New cookie == new user
+          .send({
+            title: 'ddsq',
+            price: 20
+          })
+          .expect(401)
+})
+
+it('returns a 400 if the user provides and invalid title or price', async () => {
+  const cookie = global.getAuthCookie()
+
+  const createdTicket = await request(app).post('/api/tickets').set('Cookie', cookie).send({
+    title: 'Concert',
+    price: 20
+  })
+
+  await request(app)
+    .put(`/api/tickets/${createdTicket.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 20
+    })
+    .expect(400)
+
+  await request(app)
+    .put(`/api/tickets/${createdTicket.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'fdgdfg',
+      price: -20
+    })
+    .expect(400)
+})
+
+it('updates the ticket if provided valid inputs', async () => {
+  const cookie = global.getAuthCookie()
+
+  const createdTicket = await request(app).post('/api/tickets').set('Cookie', cookie).send({
+    title: 'Concert',
+    price: 20
+  })
+
+  await request(app)
+    .put(`/api/tickets/${createdTicket.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'sdfdsf',
+      price: 42
+    })
+    .expect(200)
+
+  const ticketUpdated = await request(app)
+    .get(`/api/tickets/${createdTicket.body.id}`)
+    .send()
+
+  expect(ticketUpdated.body.title).toEqual('sdfdsf')
+  expect(ticketUpdated.body.price).toEqual(42)
+})
