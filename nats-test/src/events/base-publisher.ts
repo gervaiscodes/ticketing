8282b045b59bdcedf3d1118f60bc87f8dbe39f4d
Gervais Codes <gervaiscodes@protonmail.com>
2022-08-11 17:57:46 +0200

    Async publish

--- nats-test/src/events/base-publisher.ts
+++ nats-test/src/events/base-publisher.ts
@@ -1,4 +1,5 @@
 import { Message, Stan } from 'node-nats-streaming'
+import { resolve } from 'path'
 import { Subjects } from './subjects'
 
 interface Event {
@@ -14,9 +15,15 @@ export abstract class Publisher<T extends Event> {
     this.client = client
   }
 
-  publish(data: T['data']) {
-    this.client.publish(this.subject, JSON.stringify(data), () => {
-      console.log('Event published')
+  publish(data: T['data']): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        if (err) {
+          return reject(err)
+        }
+        console.log('Event published to subject', this.subject)
+        resolve()
+      })
     })
   }
 }
