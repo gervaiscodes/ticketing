8a060566271dc7665c124193f0d0bdcf84aac729
jd <jd@1337148.com>
2022-08-11 17:57:46 +0200

    Async publish

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,10 +7,12 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222'
 })
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS')
 
-  new TicketCreatedPublisher(stan).publish({
+  const publisher = new TicketCreatedPublisher(stan)
+
+  await publisher.publish({
     id: '123',
     title: 'Concert',
     price: 20
