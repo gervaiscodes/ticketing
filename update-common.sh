358e48e7d4753f3ae4fafac5dc423a5b59a8f3f2
jd <jd@1337148.com>
2022-08-12 18:09:31 +0200

    Update common script

---
+++ update-common.sh
@@ -0,0 +1,23 @@
+cd auth
+npm install $1
+cd ..
+
+cd tickets
+npm install $1
+cd ..
+
+cd orders
+npm install $1
+cd ..
+
+git add ./auth/package.json
+git add ./auth/package-lock.json
+
+git add ./tickets/package.json
+git add ./tickets/package-lock.json
+
+git add ./orders/package.json
+git add ./orders/package-lock.json
+
+#git commit -m "feat: update $1"
+#git push
