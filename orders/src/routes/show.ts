584eaa092088971c6c444dc424aa4ef8cca7f332
jd <jd@1337148.com>
2022-08-12 17:59:00 +0200

    Show ticket

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from 'express'
+import { Order } from '../models/order'
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@jd/ticketing-common'
 
 const router = express.Router()
 
-router.get('/api/orders/:oderId', async (req: Request, res: Response) => {
-  res.send({})
+router.get('/api/orders/:oderId', requireAuth, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.orderId).populate('ticket')
+
+  if (!order) {
+    throw new NotFoundError()
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError()
+  }
+
+  res.status(200).send(order)
 })
 
 export { router as showOrderRouter }
