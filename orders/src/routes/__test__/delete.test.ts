ad15489db2aff1352466f89b4fcf64432203d492
jd <jd@1337148.com>
2022-08-25 21:24:37 +0200

    Test order events

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { app } from '../../app'
 import { Order, OrderStatus } from '../../models/order'
 import { Ticket } from '../../models/ticket'
+import { natsWrapper } from '../../nats-wrapper'
 
 it('marks an order as cancelled', async () => {
   // create a ticket
@@ -33,4 +34,31 @@ it('marks an order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
 })
 
-it.todo('emits an event')
+it('emits an event', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
+  await ticket.save()
+
+  const user = global.getAuthCookie()
+
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id
+    })
+    .expect(201)
+
+  // make a request to cancel the order
+  const response = await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .expect(204)
+
+  // Expect event publish
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express'
 import { Order, OrderStatus } from '../models/order'
 import { requireAuth, NotFoundError, NotAuthorizedError } from '@jd/ticketing-common'
+import { OrderCancelledPublisher } from './../events/publishers/order-cancelled-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
-  const order = await Order.findById(req.params.orderId)
+  const order = await Order.findById(req.params.orderId).populate('ticket')
 
   if (!order) {
     throw new NotFoundError()
@@ -18,6 +20,13 @@ router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Res
   order.status = OrderStatus.Cancelled
 
   await order.save()
+
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id
+    }
+  })
 
   res.status(204).send(order)
 })
