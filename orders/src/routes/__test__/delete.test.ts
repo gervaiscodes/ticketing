a57036a807cdba1fa141c338026c227fb4c45fb7
jd <jd@1337148.com>
2022-08-31 18:28:51 +0200

    Revert "Comment orders tests"
    This reverts commit fa8062a3466ef128c3b7d223f0e20712e8944081.

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -4,61 +4,61 @@ import { Order, OrderStatus } from '../../models/order'
 import { Ticket } from '../../models/ticket'
 import { natsWrapper } from '../../nats-wrapper'
 
-// it('marks an order as cancelled', async () => {
-//   // create a ticket
-//   const ticket = Ticket.build({
-//     title: 'Concert',
-//     price: 20
-//   })
-//   await ticket.save()
+it('marks an order as cancelled', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
+  await ticket.save()
 
-//   const user = global.getAuthCookie()
+  const user = global.getAuthCookie()
 
-//   // make a request to create an order
-//   const { body: order } = await request(app)
-//     .post('/api/orders')
-//     .set('Cookie', user)
-//     .send({
-//       ticketId: ticket.id
-//     })
-//     .expect(201)
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id
+    })
+    .expect(201)
 
-//   // make a request to cancel the order
-//   const response = await request(app)
-//     .delete(`/api/orders/${order.id}`)
-//     .set('Cookie', user)
-//     .expect(204)
+  // make a request to cancel the order
+  const response = await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .expect(204)
 
-//   // excpect to be cancelled
-//   const updatedOrder = await Order.findById(order.id)
-//   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
-// })
+  // excpect to be cancelled
+  const updatedOrder = await Order.findById(order.id)
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
+})
 
-// it('emits an event', async () => {
-//   // create a ticket
-//   const ticket = Ticket.build({
-//     title: 'Concert',
-//     price: 20
-//   })
-//   await ticket.save()
+it('emits an event', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
+  await ticket.save()
 
-//   const user = global.getAuthCookie()
+  const user = global.getAuthCookie()
 
-//   // make a request to create an order
-//   const { body: order } = await request(app)
-//     .post('/api/orders')
-//     .set('Cookie', user)
-//     .send({
-//       ticketId: ticket.id
-//     })
-//     .expect(201)
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id
+    })
+    .expect(201)
 
-//   // make a request to cancel the order
-//   const response = await request(app)
-//     .delete(`/api/orders/${order.id}`)
-//     .set('Cookie', user)
-//     .expect(204)
+  // make a request to cancel the order
+  const response = await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .expect(204)
 
-//   // Expect event publish
-//   expect(natsWrapper.client.publish).toHaveBeenCalled()
-// })
+  // Expect event publish
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -3,57 +3,57 @@ import { app } from '../../app'
 import { Order } from '../../models/order'
 import { Ticket } from '../../models/ticket'
 
-// it('fetches the order', async () => {
-//   // create a ticket
-//   const ticket = Ticket.build({
-//     title: 'Concert',
-//     price: 20
-//   })
-//   await ticket.save()
+it('fetches the order', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
+  await ticket.save()
 
-//   const user = global.getAuthCookie()
+  const user = global.getAuthCookie()
 
-//   // make a request to build an order with this ticket
-//   const { body: order } = await request(app)
-//     .post('/api/orders')
-//     .set('Cookie', user)
-//     .send({
-//       ticketId: ticket.id
-//     })
-//     .expect(201)
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id
+    })
+    .expect(201)
 
-//   // make a request to fetch the order
-//   const { body: fetchedOrder } = await request(app)
-//     .get(`/api/orders/${order.id}`)
-//     .set('Cookie', user)
-//     .expect(200)
+  // make a request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .expect(200)
 
-//   expect(fetchedOrder.id).toEqual(order.id)
-// })
+  expect(fetchedOrder.id).toEqual(order.id)
+})
 
-// it('returns an error if trying to fetch another users order', async () => {
-//   // create a ticket
-//   const ticket = Ticket.build({
-//     title: 'Concert',
-//     price: 20
-//   })
-//   await ticket.save()
+it('returns an error if trying to fetch another users order', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
+  await ticket.save()
 
-//   const user = global.getAuthCookie()
+  const user = global.getAuthCookie()
 
-//   // make a request to build an order with this ticket
-//   const { body: order } = await request(app)
-//     .post('/api/orders')
-//     .set('Cookie', user)
-//     .send({
-//       ticketId: ticket.id
-//     })
-//     .expect(201)
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id
+    })
+    .expect(201)
 
-//   // make a request to fetch the order
-//   const otherUser = global.getAuthCookie()
-//   const { body: fetchedOrder } = await request(app)
-//     .get(`/api/orders/${order.id}`)
-//     .set('Cookie', otherUser)
-//     .expect(401)
-// })
+  // make a request to fetch the order
+  const otherUser = global.getAuthCookie()
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', otherUser)
+    .expect(401)
+})
