cdfd2c6fb81438d1bf8499ed28926c9b4db89a1b
jd <jd@1337148.com>
2022-09-04 14:25:11 +0200

    Order mongoose-update-if-current

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
 import { OrderStatus } from '@jd/ticketing-common'
 import { TicketDoc } from './ticket'
 
@@ -21,6 +22,7 @@ interface OrderDoc extends mongoose.Document {
   status: OrderStatus
   expiresAt: Date
   ticket: TicketDoc
+  version: number
 }
 
 const orderSchema = new mongoose.Schema({
@@ -49,6 +51,9 @@ const orderSchema = new mongoose.Schema({
     }
   }
 })
+
+orderSchema.set('versionKey', 'version')
+orderSchema.plugin(updateIfCurrentPlugin)
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs)
