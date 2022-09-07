df5edcac981350cbccb9f627ab3f9e870bd84cb0
jd <jd@1337148.com>
2022-09-07 17:18:54 +0200

    Add expiration service to update-common script

--- update-common.sh
+++ update-common.sh
@@ -10,6 +10,10 @@ cd orders
 npm install $1
 cd ..
 
+cd expiration
+npm install $1
+cd ..
+
 git add ./auth/package.json
 git add ./auth/package-lock.json
 
@@ -18,5 +22,8 @@ git add ./tickets/package-lock.json
 
 git add ./orders/package.json
 git add ./orders/package-lock.json
+
+git add ./expiration/package.json
+git add ./expiration/package-lock.json
 
 git commit -m "Bump to $1"
