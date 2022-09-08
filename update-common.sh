1773f52666b12621e4fec12bb900e5196c89c38a
Gervais Codes <gervaiscodes@protonmail.com>
2022-09-08 22:47:59 +0200

    Intialize payments service

--- update-common.sh
+++ update-common.sh
@@ -14,6 +14,10 @@ cd expiration
 npm install $1
 cd ..
 
+cd expiration
+npm install $1
+cd ..
+
 git add ./auth/package.json
 git add ./auth/package-lock.json
 
@@ -25,5 +29,8 @@ git add ./orders/package-lock.json
 
 git add ./expiration/package.json
 git add ./expiration/package-lock.json
+
+git add ./payments/package.json
+git add ./payments/package-lock.json
 
 git commit -m "Bump to $1"
