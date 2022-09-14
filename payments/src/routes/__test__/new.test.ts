fd8f1a38049b48fd83b76ab910dd800a8d6541a5
jd <jd@1337148.com>
2022-09-14 19:23:02 +0200

    Test Stripe charge creation

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,9 @@ import mongoose from 'mongoose'
 import { app } from '../../app'
 import { Order } from '../../models/order'
 import { natsWrapper } from '../../nats-wrapper'
+import { stripe } from '../../stripe'
+
+jest.mock('../../stripe')
 
 it('returns an error if the order does not exist', async () => {
   await request(app)
@@ -57,4 +60,33 @@ it('returns an error if the order is cancelled', async () => {
       orderId: order.id
     })
     .expect(400)
+})
+
+it('returns a 204 with valid inputs', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString()
+
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created
+  })
+
+  await order.save()
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.getAuthCookie(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id
+    })
+    .expect(201)
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0]
+
+  expect(chargeOptions.source).toEqual('tok_visa')
+  expect(chargeOptions.amount).toEqual(20 * 100)
+  expect(chargeOptions.currency).toEqual('usd')
 })
