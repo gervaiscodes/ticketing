c43ffb315e77f12878638d04843e6be65ba1a549
Gervais Codes <gervaiscodes@protonmail.com>
2022-09-16 18:26:37 +0200

    Display list of tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,35 @@
-const LandingPage = ({ currentUser }) => {
-  console.log(currentUser)
-  return <h1>Landing page</h1>
+const LandingPage = ({ tickets }) => {
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return { }
+  const { data } = await client.get('/api/tickets')
+
+  return { tickets: data }
 }
 
 export default LandingPage
